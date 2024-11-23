6385e94c31bdec53e751fcae5c07271c3d2f0a83
ayas <ahmdyaasiin@student.ub.ac.id>
2024-11-24 01:52:45 +0700

    chore(migrations): add transaction_item_ratings migration file

---
+++ database/migrations/2024_11_23_180201_transaction_item_ratings.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction_item_ratings', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->text('feedback')->nullable();
+            $table->smallInteger('start');
+            $table->timestamps();
+            $table->foreignUlid('transaction_item_id')->constrained('transaction_items')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_item_ratings');
+    }
+};
