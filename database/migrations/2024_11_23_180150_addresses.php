73bf4ed772907ae25d95e3f15e68619052363fe8
ayas <ahmdyaasiin@student.ub.ac.id>
2024-11-24 01:43:31 +0700

    chore(migrations): add addresses migration file

---
+++ database/migrations/2024_11_23_180150_addresses.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->string('name');
+            $table->string('detailed_address');
+            $table->string('city');
+            $table->string('state');
+            $table->char('postal_code', 6);
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->boolean('is_primary')->default(false);
+            $table->timestamps();
+            $table->foreignUlid('user_id')->constrained('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
