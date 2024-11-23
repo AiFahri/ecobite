29e67b0020b420186b1cb55e3578bacd95f0985b
ayas <ahmdyaasiin@student.ub.ac.id>
2024-11-24 01:33:41 +0700

    chore(migrations): update data type on description column

--- database/migrations/2024_11_23_180025_vouchers.php
+++ database/migrations/2024_11_23_180025_vouchers.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('vouchers', function (Blueprint $table) {
             $table->ulid('id')->primary();
             $table->string('name');
-            $table->string('description');
+            $table->text('description')->nullable();
             $table->boolean('is_percentage')->default(false);
             $table->bigInteger('amount');
             $table->timestamps();
