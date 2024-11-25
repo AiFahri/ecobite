9522764b10e20604b6ba5bc754c207b0e1123b19
ayas <ahmdyaasiin@student.ub.ac.id>
2024-11-25 13:32:31 +0700

    feat(routes): add Google and Facebook OAuth routes to authentication

--- routes/auth.php
+++ routes/auth.php
@@ -2,18 +2,21 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\OauthController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
+    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
     Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-        ->name('login');
-
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+    Route::get('/auth/google', [OauthController::class, 'googleLogin'])->name('google.login');
+    Route::get('/auth/google/callback', [OauthController::class, 'googleCallback']);
+
+    Route::get('/auth/facebook', [OauthController::class, 'facebookLogin'])->name('facebook.login');
+    Route::get('/auth/facebook/callback', [OauthController::class, 'facebookCallback']);
 });
 
 Route::middleware('auth')->group(function () {
