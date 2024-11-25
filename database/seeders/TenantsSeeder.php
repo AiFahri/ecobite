39607b6112fedf744a0586dd025495261671fff7
ayas <ahmdyaasiin@student.ub.ac.id>
2024-11-25 20:56:40 +0700

    feat(seeder): add TenantsSeeder to populate tenants with fake data

---
+++ database/seeders/TenantsSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+
+class TenantsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create('id_ID');
+
+        $tenantTypes = DB::table('tenant_types')->pluck('id')->toArray();
+
+        foreach (range(1, 10) as $index) {
+            DB::table('tenants')->insert([
+                'id' => (string) \Illuminate\Support\Str::ulid(),
+                'name' => $faker->company,
+                'is_verified' => $faker->boolean,
+                'city' => $faker->city,
+                'state' => $faker->state,
+                'postal_code' => $faker->postcode,
+                'latitude' => $faker->latitude(-10, 5),
+                'longitude' => $faker->longitude(95, 141),
+                'created_at' => now(),
+                'updated_at' => now(),
+                'tenant_type_id' => $faker->randomElement($tenantTypes),
+            ]);
+        }
+    }
+}
