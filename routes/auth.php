00aa5f522117537cbf7a5ef83f8f98bea2c84ad4
ayas <ahmdyaasiin@student.ub.ac.id>
2024-12-01 01:46:24 +0700

    feat(routes): add cart route to authenticated user group

--- routes/auth.php
+++ routes/auth.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\OauthController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Route;
@@ -27,4 +28,6 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::get('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
+
+    Route::get('/cart', [CartController::class, 'show'])->name('cart');
 });
