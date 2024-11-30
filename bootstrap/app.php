947a7d7b0cf95109b777bae5f055587aadda559f
ayas <ahmdyaasiin@student.ub.ac.id>
2024-11-30 23:30:47 +0700

    feat(app): configure middleware alias for PaymentMiddleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,8 +7,8 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -20,6 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
+    })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias(['payment' => \App\Http\Middleware\PaymentMiddleware::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
