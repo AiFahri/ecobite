21453896c74713cfcfaa6fe526ebd4c74176ccf7
ayas <ahmdyaasiin@student.ub.ac.id>
2024-12-02 07:37:10 +0700

    fix: adjust rendering condition for product rating in ProductCard component

--- resources/js/Components/Catalog/ProductCard.jsx
+++ resources/js/Components/Catalog/ProductCard.jsx
@@ -84,7 +84,7 @@ const ProductCard = ({ product, isWishlist = false, onToggleWishlist }) => {
                         />
                     )}
                 </span>
-                {product.rating && (
+                {(
                     <div className="flex items-center mt-2">
                         {[...Array(5)].map((_, i) => (
                             <span
